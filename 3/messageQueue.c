347efd3d7189aaa0caae24f0ce2a2771810ff173
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2020-04-18 23:26:05 +0800

    Update but it is still not useful.

--- 3/messageQueue.c
+++ 3/messageQueue.c
@@ -12,20 +12,22 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+#define TXTLEN (512)
+
 struct msg
 {
-    long msg_types;
-    char msg_buf[511];
+    long msg_type;
+    char msg_buf[TXTLEN];
 };
 
 int main()
 {
     printf("作者：181110305董成相\n");
     int qid;
-    // key_t key;
     pid_t pid;
-    size_t len;
     struct msg pmsg;
+
+    pid = fork();
 
     if (qid = msgget((key_t)1234, IPC_CREAT | 0666) < 0)
     {
@@ -34,28 +36,33 @@ int main()
     }
     printf("创建、打开的消息队列号是：%d\n", qid);
 
-    /* fork two process */
-    // pid = fork();
-    sprintf(pmsg.msg_buf, "hello! this is: %d\n", getpid());
-    // pmsg.msg_types = getpid();
-    len = strlen(pmsg.msg_buf);
-    if (msgsnd(qid, (void*)&pmsg, len, 0) < 0)
+    if (pid == 0)
     {
-        perror("msgsnd\n");
-        exit(1);
+        sprintf(pmsg.msg_buf, "hello! this is: %d\n", getpid());
+        pmsg.msg_type = 1;
+        if (msgsnd(qid, (void*)&pmsg, TXTLEN, 0) < 0)
+        {
+            perror("msgsnd\n");
+            exit(1);
+        }
+        printf("successfully send a message to the queue: %d\n", qid);
     }
-    printf("successfully send a message to the queue: %d\n", qid);
-    if (msgrcv(qid, (void*)&pmsg, 512, 1, 0) < 0)
+
+    if (pid < 0)
     {
-        perror("msgrcv\n");
-        exit(1);
+        printf("Hello, I'm the parent!\n");
+        pmsg.msg_type = 0;
+        if (msgrcv(qid, (void*)&pmsg, TXTLEN, 0, 0) < 0)
+        {
+            perror("msgrcv\n");
+            exit(1);
+        }
+        printf("读取的消息是：%s\n", pmsg.msg_buf);
+        if (msgctl(qid, IPC_RMID, NULL) < 0)
+        {
+            perror("msgctl\n");
+            exit(1);
+        }
     }
-    printf("读取的消息是：%s\n", pmsg.msg_buf);
-    if (msgctl(qid, IPC_RMID, NULL) < 0)
-    {
-        perror("msgctl\n");
-        exit(1);
-    }
-    
     return 0;
 }
