f52c5b1e1a9e7d6e27ac7b376157123c4c795d8f
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2020-05-01 19:46:54 +0800

    remove unneeded include library: <sys/types.h> <sys/stat.h> <fcntl.h> <errno.h> <stropts.h> <time.h> <string.h> <strings.h> <sys/ipc.h>.

--- 3/messageQueue.c
+++ 3/messageQueue.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
-#include <stropts.h>
-#include <time.h>
-#include <string.h>
-#include <strings.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
 
 #define TXTLEN (512)
